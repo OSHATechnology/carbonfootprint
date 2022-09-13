606a9130cbef7fad2968b698fd3db3dc11e8b157
OSHATechnology <oshatechnology@gmail.com>
2022-09-13 14:34:57 +0700

    update table

---
+++ database/migrations/2022_09_13_040030_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_plant');
+            $table->bigInteger('id_emisi');
+            $table->string('welding',20);
+            $table->string('painting',20);
+            $table->string('assy',20);
+            $table->string('press',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
