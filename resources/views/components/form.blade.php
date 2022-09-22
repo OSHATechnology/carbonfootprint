d856a163be0ec5ac4743ccf3a6f2c2707750b657
deaagnia <deaagnia@gmail.com>
2022-09-22 10:37:17 +0700

    fix dashboard

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -3,10 +3,10 @@
         <div class="pd-20 card-box mb-30">
             <div class="clearfix mb-20">
                 <div class="pull-left">
-                    <h4 class="text-green h4">Input</h4>
+                    <h4 class="text-red-50 h4">Input</h4>
                 </div>
                 <div class="pull-right">
-                    <a href="#basic-table" class="btn btn-success btn-sm scroll-click reset" rel="content-y" data-toggle="collapse" role="button"><i class="icon-copy fa fa-refresh" aria-hidden="true"></i> Refresh</a>
+                    <a href="#basic-table" class="btn btn-danger btn-sm scroll-click reset" rel="content-y" data-toggle="collapse" role="button"><i class="icon-copy fa fa-refresh" aria-hidden="true"></i> Refresh</a>
 
                 </div>
             </div>
@@ -42,7 +42,7 @@
 
             <div class="row">
                 <div class="col-md-12">
-                    <a href="#basic-table" class="btn btn-success btn-calculate" type="button" style="width:100%"> <i class="icon-copy fa fa-calculator" aria-hidden="true"></i> Calculate</a>
+                    <a href="#basic-table" class="btn btn-danger btn-calculate" type="button" style="width:100%"> <i class="icon-copy fa fa-calculator" aria-hidden="true"></i> Calculate</a>
                 </div>
             </div>
         </div>
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -6,9 +6,9 @@
 	<title>Calcuation Carbon Footprint</title>
 
 	<!-- Site favicon -->
-	<link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/vendors')}}/images/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/vendors')}}/images/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/vendors')}}/images/favicon-16x16.png">
+	<!-- <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/vendors')}}/images/apple-touch-icon.png"> -->
+	<link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/src')}}/images/toyota-logo-loading.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/src')}}/images/toyota-logo-loading.png">
 
 	<!-- Mobile Specific Metas -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
